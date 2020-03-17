04e3b0187b29ecc3976aee8b2f10993013330719
SG0304986 <abhinav.s@sabre.com>
2020-03-17 22:12:48 +0530

    Updates

---
+++ precache-manifest.bc18f95e8b3c1f791c40bd890de30732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "1b5a4c1eba4bf6395496",
+    "url": "/my-app/static/js/main.1b5a4c1e.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/my-app/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "1b5a4c1eba4bf6395496",
+    "url": "/my-app/static/css/main.cb764624.chunk.css"
+  },
+  {
+    "revision": "e1de9bc629d0832189edb4628d567dfd",
+    "url": "/my-app/index.html"
+  }
+];
