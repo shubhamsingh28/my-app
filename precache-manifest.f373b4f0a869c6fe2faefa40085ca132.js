8b388b2562967cfddffa1e7a4a3832d345389c85
MastersAbh <mastersabhinav@gmail.com>
2020-03-25 21:21:25 +0530

    Updates

---
+++ precache-manifest.f373b4f0a869c6fe2faefa40085ca132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "615dc04be3ec1fd308e2",
+    "url": "/my-app/static/js/main.615dc04b.chunk.js"
+  },
+  {
+    "revision": "a91136e6549fb2ef3709",
+    "url": "/my-app/static/js/1.a91136e6.chunk.js"
+  },
+  {
+    "revision": "615dc04be3ec1fd308e2",
+    "url": "/my-app/static/css/main.8c899f47.chunk.css"
+  },
+  {
+    "revision": "a91136e6549fb2ef3709",
+    "url": "/my-app/static/css/1.e92c871f.chunk.css"
+  },
+  {
+    "revision": "e868098385f734a9826e93f9ad053383",
+    "url": "/my-app/index.html"
+  }
+];
